f7c846a695f1deba26896b58ffacb1577d93aaf1
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-05-12 15:55:04 -0500

    Organización

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-from config import TOKEN
-import bot
-
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
+import help
+import graph
+import rr
 
 def main():
+    # TOKEN CAMILO = 1664245450:AAEh6R8xK_iSJ58-TQzI144h_xvQZyRMNY0
     # Establecemos una conexión entre nuestro programa y el bot.
     updater = Updater("TOKEN", use_context=True)  # Insertemos el Token del bot.
     dp = updater.dispatcher
-
+    
     # Establecer los comandos que ejecutará el bot.
-    dp.add_handler(CommandHandler("start", bot.start))
-    dp.add_handler(CommandHandler("ayuda", bot.ayuda))
-    dp.add_handler(CallbackQueryHandler(bot.menu, pattern="op1"))
-    dp.add_handler(CallbackQueryHandler(bot.menu, pattern="op2"))
-    dp.add_handler(CallbackQueryHandler(bot.menu, pattern="op3"))
-    dp.add_handler(CallbackQueryHandler(bot.menu, pattern="op4"))
-    dp.add_handler(CommandHandler("doc", bot.documento))
-    dp.add_handler(CommandHandler("img", bot.image))
-    dp.add_handler(CommandHandler("grafo", bot.grafo))
-    dp.add_handler(CommandHandler("sec", bot.secuencia_prime))
-    dp.add_handler(CommandHandler("fibonacci", bot.fibonacci))
+    dp.add_handler(CommandHandler("start", help.start))
+    dp.add_handler(CommandHandler("help", help.help))
+
+    dp.add_handler(CallbackQueryHandler(help.menu, pattern="RR"))
+    dp.add_handler(CallbackQueryHandler(help.menu, pattern="fibonacci"))
+
+    dp.add_handler(CommandHandler("fibonacci", help.fibonacci))
+
+    dp.add_handler(ConversationHandler(
+        entry_points=[
+            CommandHandler("sec",rr.input_command_secuencia),
+            CallbackQueryHandler(callback = rr.input_callback_secuencia, pattern="sec")
+        ],
+        states = {
+            rr.INPUT_LISTA: [MessageHandler(Filters.text, rr.input_polinomio)],
+            rr.INPUT_CI: [MessageHandler(Filters.text, rr.input_ci)],
+            rr.INPUT_I: [MessageHandler(Filters.text, rr.input_i0)],
+        },
+        fallbacks = [],
+    ))
+
+    dp.add_handler(ConversationHandler(
+        entry_points=[
+            CommandHandler("grafo", graph.input_command_grafo),
+            CallbackQueryHandler(callback = graph.input_callback_grafo, pattern="grafo")
+        ],
+        states = {
+            graph.INPUT_VERTICES: [MessageHandler(Filters.text, graph.input_vertices)],
+            graph.INPUT_ARISTAS: [MessageHandler(Filters.text, graph.input_aristas)],
+            graph.INPUT_K: [MessageHandler(Filters.text, graph.input_k)],
+        },
+        fallbacks = [],
+    ))
+    
 
     # Iniciamos el bot.
     updater.start_polling()
