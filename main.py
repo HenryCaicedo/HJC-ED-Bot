a6c78e69978273a144ab95bd4d0f81751b8a5332
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2021-05-18 17:55:37 -0500

    Token cambiado

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     # TOKEN CAMILO = 1664245450:AAEh6R8xK_iSJ58-TQzI144h_xvQZyRMNY0
     # TOKEN HENRY = 1600741375:AAGjGuD_jOvTQvw7qbqFcGF9zuQTM3OztA8
     # Establecemos una conexión entre nuestro programa y el bot.
-    updater = Updater("1664245450:AAEh6R8xK_iSJ58-TQzI144h_xvQZyRMNY0", use_context=True)  # Insertemos el Token del bot.
+    updater = Updater("1600741375:AAGjGuD_jOvTQvw7qbqFcGF9zuQTM3OztA8", use_context=True)  # Insertemos el Token del bot.
     dp = updater.dispatcher
     
     # Establecer los comandos que ejecutará el bot.
